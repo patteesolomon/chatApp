718297cfd9b6a5d29d1a80c4f5011cb40f6fe672
patteesolomon <patteesolomon@gmail.com>
2023-03-02 16:03:33 -0600

    optical illusion

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var ctx = canvas.getContext('2d');
 		var x = 0;
 		var y = 0; // screen starting point
 		var next = 1;
-		var h = .360;
+		var h = .33333333367;
 		var prev = 0;
 		ctx.beginPath();
-		ctx.moveTo(500 + x , 500 + y);
-		for (var i = 0; i <= 100; i++)
+		ctx.moveTo(550+x , 400 + y);
+		for (var i = 0; i <= 70; i++)
 		{
 			if (next == prev)
 			{
@@ -30,7 +30,7 @@ var ctx = canvas.getContext('2d');
 			
 				 // draw a line
 			// x, y check
-			ctx.quadraticCurveTo(i % ctx.arc(660+x, 300+y, i*i, i, i), 0, x, y);
+			ctx.quadraticCurveTo(i % ctx.arc(x+560, y+390, i*10, i, i), y, x, y);
 			// change x,y using Lerp
 			ctx.stroke();
 		}
